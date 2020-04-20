d6ece9019fe2a35fdc67d98c67116f11372c5483
Lenny Makori <makorilenny28@gmail.com>
2020-04-20 16:19:20 +0300

    create test_init function to test user authentication

--- account-test.py
+++ account-test.py
@@ -12,4 +12,23 @@ class TestAccount(unittest.TestCase):
 
         self.new_user = User("Lenny", "Makori", "moringastudent001")
 
+    def tearDown(self):
+        Credentials.accounts_list = []
+
+    def test_init(self):
+        '''
+        test case to check if the user is logged in properly
+        '''
+        self.assertEqual(self.new_user.user_first_name, "Lenny")
+        self.assertEqual(self.new_user.user_last_name, "Makori")
+        self.assertEqual(self.new_user.login_password, "moringastudent001")
+
+# if __name__ == "__main__":
+#     unittest.main()
+
+
     
+if __name__ == "__main__":
+    unittest.main()
+
+        
